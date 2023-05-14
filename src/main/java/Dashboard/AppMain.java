8d0fb414be7e896de96db804045b881c1396d2ca
Willy <guillecg13@gmail.com>
2023-05-14 12:25:46 +0200

    Changed: remove spaces

--- src/main/java/Dashboard/AppMain.java
+++ src/main/java/Dashboard/AppMain.java
@@ -4,11 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.sql.SQLException;
-
-import static Dashboard.utils.DataBase.closeBD;
 import static Dashboard.utils.DataBase.createDB;
 
 public class AppMain extends Application {
