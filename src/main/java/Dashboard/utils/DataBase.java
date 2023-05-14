d9b9db0dfbee30f7015f80f150d413850830d91c
Willy <guillecg13@gmail.com>
2023-05-14 12:19:18 +0200

    Added: documentation

--- src/main/java/Dashboard/utils/DataBase.java
+++ src/main/java/Dashboard/utils/DataBase.java
@@ -1,6 +1,5 @@
 package Dashboard.utils;
 
-import javafx.application.Application;
 import java.sql.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,31 +7,80 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 
 import static java.lang.Integer.parseInt;
 
+/**
+ * @author Chen, S. Folgueras y Willy
+ * @version 1.0.0
+ * 12/05/2023
+ */
+
+/**
+ * Clase que contiene toda la lógica relacionada con la base de datos
+ */
 public class DataBase {
     // Atributos
+    /**
+     * Titulo del libro
+     */
     private String title;
+    /**
+     * Autor del libro
+     */
     private String author;
+    /**
+     * Fecha de publicacón del libro
+     */
     private Date publish_day;
+    /**
+     * Cantidad que tenemos del libro
+     */
     private int book_quantity;
+    /**
+     * Sinopsis del libro
+     */
     private String sinopsis;
+    /**
+     * Email del usuario
+     */
     private String user_email;
+    /**
+     * Fecha de alquiler del libro
+     */
     private Date rent_day;
+    /**
+     * Fecha de devolución del libro
+     */
     private Date return_day;
 
+    /**
+     * Atributo estatico del tipo Statement
+     * para ejecutar las querys de la base de datos
+     */
     public static Statement smt;
+    /**
+     * Atributo estatico del tipo Connection
+     * para establecer la consexión con la base de datos
+     */
     public static Connection dbConnection;
 
-    // Constructors
+    // Constructores
+    /**
+     * Constructor por defecto
+     */
     public DataBase() {
     }
-
+    /**
+     * Constructor para crear los datos del libro
+     * @param title titulo del libro
+     * @param author autor del libro
+     * @param publish_day publicación del libro
+     * @param book_quantity cantidad del libro
+     * @param sinopsis sinopsis del libro
+     */
     // Books table constructor
     public DataBase(String title, String author, Date publish_day, int book_quantity, String sinopsis) {
         this.title = title;
@@ -42,7 +90,13 @@ public class DataBase {
         this.sinopsis = sinopsis;
     }
 
-    // Rent table constructor
+    /**
+     * Constructor para crear los del alquiler
+     * @param title titulo del libro
+     * @param rent_day día de alquiler del libro
+     * @param return_day día de devolución del libro
+     * @param email email del usuario que ha alquilado el libro
+     */
     public DataBase(String title, Date rent_day, Date return_day, String email) {
         this.title = title;
         this.rent_day = rent_day;
@@ -51,55 +105,73 @@ public class DataBase {
     }
 
     // Getters
+    /**
+     * Método que retorna el título del libro
+     */
     public String getTitle() {
         return title;
     }
-
+    /**
+     * Método que retorna el autor del libro
+     */
     public String getAuthor() {
         return author;
     }
-
+    /**
+     * Método que retorna el día de publicación del libro
+     */
     public Date getPublish_day() {
         return publish_day;
     }
-
+    /**
+     * Método que retorna la cantidad que tenemos del libro
+     */
     public int getBook_quantity() {
         return book_quantity;
     }
-
+    /**
+     * Método que retorna la sinopsis del libro
+     */
     public String getSinopsis() {
         return sinopsis;
     }
-
+    /**
+     * Método que retorna el email del usuario
+     */
     public String getUser_email() {
         return user_email;
     }
-
+    /**
+     * Método que retorna la fecha de alquiler del libro
+     */
     public Date getRent_day() {
         return rent_day;
     }
-
+    /**
+     * Método que retorna la fecha de devolución del libro
+     */
     public Date getReturn_day() {
         return return_day;
     }
-
+    /**
+     * Método para crear la base de datos
+     */
     public static void createDB () throws SQLException {
         try {
+            // Establecemos conexión a base de datos
             dbConnection= DriverManager.getConnection("jdbc:mysql://localhost:3306/","library", "Cide2023");
-            System.out.println("Conexión ha sido establecida");
             smt = dbConnection.createStatement();
 
-            //  Eliminate Data Base just in case if we need to.
+            // Eliminar la Base de Datos por si acaso
             smt.executeUpdate("drop database if exists library");
-            //  smt.execute("Drop database library");
 
-            //Create database named library
+            // Crear base de datos llamada biblioteca
             smt.executeUpdate("create database if not exists library");
 
-            //Use the database which we just created
+            // Utilizar la base de datos que acabamos de crear
             smt.executeUpdate("use library");
 
-            //Create all tables necessary:
+            // Crear todas las tablas necesarias:
             smt.executeUpdate("create table if not exists  lib_user(" +
                     "    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY," +
                     "    user_email VARCHAR(50) NOT NULL," +
@@ -149,7 +221,7 @@ public class DataBase {
                     "    FOREIGN KEY(book_id) REFERENCES lib_book(book_id)" +
                     ")");
 
-            //Add information to table book:
+           //Agregar información a la tabla libro:
            smt.executeUpdate("insert into lib_book values(1,'Cien años de soledad','Gabriel García Márquez','1967-5-30',false," +
                     "'La novela cuenta la historia de la familia Buendía a lo largo de varias generaciones en el pueblo ficticio de Macondo. A través de la vida de los Buendía, García Márquez explora temas como la soledad, el amor y la mortalidad, y utiliza técnicas narrativas como el realismo mágico para crear una obra maestra de la literatura latinoamericana.',25);");
 
@@ -169,7 +241,7 @@ public class DataBase {
                     " mientras se embarca en una aventura para recuperar un tesoro robado por el dragón Smaug. A través de la narrativa, Tolkien explora temas como la valentía," +
                     " la amistad y la naturaleza humana.',15);");
 
-            //Add information to table author:
+            // Agregar información a la tabla autor:
             smt.executeUpdate("insert into lib_author values(1,'Gabriel','García','Márquez','1927-1-1','2014-1-1')");
             smt.executeUpdate("insert into lib_author values(2,'Herman','Melville',null,'1819-1-1','1891-1-1')");
             smt.executeUpdate("insert into lib_author values(3,'Francis','Fitzgerald','Key','1896-1-1','1940-1-1')");
@@ -177,7 +249,7 @@ public class DataBase {
             smt.executeUpdate("insert into lib_author values(5,'Louisa','Alcott','May','1832-1-1','1888-1-1')");
             smt.executeUpdate("insert into lib_author values(6,'John Ronald Reuel','Tolkien',null,'1892-1-1','1973-1-1')");
 
-            //Add information to table author_book
+            // Agregar información a la tabla:
             smt.executeUpdate("insert into lib_author_book values(1,1,1)");
             smt.executeUpdate("insert into lib_author_book values(2,2,2)");
             smt.executeUpdate("insert into lib_author_book values(3,3,3)");
@@ -185,24 +257,32 @@ public class DataBase {
             smt.executeUpdate("insert into lib_author_book values(5,5,5)");
             smt.executeUpdate("insert into lib_author_book values(6,6,6)");
 
-            //this is a test to insert to table user
+            //Agregar información a la tabla usuarios:
             smt.executeUpdate("insert into lib_user values(1,'test@gmail.com','test')");
             smt.executeUpdate("insert into lib_user values(2,'willy@gmail.com','willy')");
 
-            //Add information to table lib_rent
+            //Agregar información a la tabla de alquiler:
             smt.executeUpdate("insert into lib_rent(user_id, book_id, rent_day,return_day,rent_quantity) values(1,1,'2023-06-12','2023-07-20',1)");
 
-
+            // Cerrar conexión con la Dase de Datos
             dbConnection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Método que devuelve los datos de una consulta a la Base de Datos
+     * @param query query de la consulta
+     */
     public static ResultSet getData(String query) throws SQLException{
         ResultSet data = smt.executeQuery(query);
         return data;
     }
 
+    /**
+     * Método para iniciar la conexión a Base de Datos
+     */
     public static void initDB() throws SQLException {
         try {
             dbConnection= DriverManager.getConnection("jdbc:mysql://localhost:3306/library","library", "Cide2023");
@@ -212,6 +292,9 @@ public class DataBase {
         }
     }
 
+    /**
+     * Método para cerrar la conexión con la Base de Datos
+     */
     public static void closeBD() throws SQLException {
         try {
             dbConnection.close();
@@ -219,7 +302,16 @@ public class DataBase {
             throw new RuntimeException(e);
         }
     }
-  
+
+    /**
+     * Método para mostra la información del libro en la tabla
+     * @param title columna del titulo del libro
+     * @param author columna del autor del libro
+     * @param publish_day columna de la fecha de publicación del libro
+     * @param book_quantity columna de la cantidad del libro
+     * @param sinopsis columna de sinopsis del libro
+     * @param table tabla donde van los datos del libro
+     */
     public static void showAllBooks(TableColumn<DataBase, String> title, TableColumn<DataBase, String> author, TableColumn<DataBase, Date> publish_day, TableColumn<DataBase, Integer> book_quantity, TableColumn<DataBase, String> sinopsis, TableView<DataBase> table) throws SQLException{
         try {
             initDB();
@@ -243,7 +335,15 @@ public class DataBase {
         }
     }
 
-   public static void rentBooksTable(TableColumn<DataBase, String> title, TableColumn<DataBase, Date> rentDate, TableColumn<DataBase, Date> returnDate, TableColumn<DataBase, String> email, TableView<DataBase> table) throws SQLException {
+    /**
+     * Método para mostra la información del alquiler del libro en la tabla
+     * @param title columna del titulo del libro
+     * @param rentDate columna de la fecha de alquiler del libro
+     * @param returnDate columna de la fecha de devolución del libro
+     * @param email columna del email del usuario que ha alquilado el libro
+     * @param table tabla donde van los datos del alquiler
+     */
+    public static void rentBooksTable(TableColumn<DataBase, String> title, TableColumn<DataBase, Date> rentDate, TableColumn<DataBase, Date> returnDate, TableColumn<DataBase, String> email, TableView<DataBase> table) throws SQLException {
            initDB();
            String query = "select b.title, r.rent_day, r.return_day, u.user_email from lib_rent r inner join lib_user u on r.user_id = u.user_id inner join lib_book b on b.book_id = r.book_id;";
            ResultSet rs = smt.executeQuery(query);
@@ -261,6 +361,11 @@ public class DataBase {
            closeBD();
     }
 
+    /**
+     * Método para dar de alta usuarios
+     * @param email email del usuario
+     * @param password contraseña del usuario
+     */
     public static void insertUser(String email, String password) throws SQLException {
         initDB();
         String query = ("insert into lib_user (user_email, user_password) values('"+ email +"','"+ password +"');");
@@ -271,7 +376,15 @@ public class DataBase {
         alert.setHeaderText("Registro completado correctamente");
         alert.showAndWait();
     }
-  
+
+    /**
+     * Método para insertar libros en la Base de Datos
+     * @param title columna del titulo del libro
+     * @param author columna del autor del libro
+     * @param sinopsis columna de sinopsis del libro
+     * @param date columna de la fecha de publicación del libro
+     * @param quantity columna de la cantidad del libro
+     */
     public static void insertBook(String title, String author, String sinopsis, String date, String quantity) throws ParseException, SQLException {
         initDB();
         int quantityDB = parseInt(quantity);
